3223ea1b01a3e524a17f808b5e7567cc3fe45f38
rajazulqarnain <rajazulqarnain@192.168.1.83>
2020-08-24 22:30:30 -0700

    utl

---
+++ src/utils/FlightAppConstants.java
@@ -0,0 +1,21 @@
+package utils;
+
+/**
+ * Convenience class to hold constants for CP1953 project.
+ *
+ * @author smonk
+ * @version 24-Sep-2015
+ */
+public class FlightAppConstants {
+
+    public static final String[] WEEKDAYS = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
+    public static final String ECONOMY = "Economy";
+    public static final String BUSINESS = "Business";
+    public static final String FIRST = "First";
+    
+    public static final int AIRPORTS_NOT_LOADED = 0;
+    public static final int AIRPORTS_LOADED = 1;
+    public static final int AIRPORT_SELECTED = 2;
+
+} // end class FlightAppConstants
