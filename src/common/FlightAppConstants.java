adc9ec826c301aec8a73396f06cdb25e4d39776e
rajazulqarnain <rajazulqarnain@192.168.1.83>
2020-08-24 20:23:32 -0700

    Flight App Constants

---
+++ src/common/FlightAppConstants.java
@@ -0,0 +1,13 @@
+package common;
+
+public class FlightAppConstants {
+
+    public static final int AIRPORTS_NOT_LOADED = 0;
+    public static final int AIRPORTS_LOADED = 1;
+    public static final int AIRPORT_SELECTED = 2;
+    
+    public static final int AIRCRAFTS_NOT_LOADED = 0;
+    public static final int AIRCRAFTS_LOADED = 1;
+    public static final int AIRCRAFTS_SELECTED = 2;
+    
+}
